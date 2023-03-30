e6ac6e12fc4fa8d2730c28b27792ff76f9654a5b
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-03-30 17:20:22 +0200

    updtt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
   }, []);
   return (
     <div className="  h-screen text-white hero__container  mx-auto ">
-      <div className="w-[98%] mx-auto sm:w-full sm:px-2 pt-24 pb-10 sm:pt-20  ">
+      <div className="w-[98%] mx-auto sm:w-full pt-24 pb-10 sm:px-2  sm:pt-20 sm:pb-12  ">
         <div className="h-[63vh] bg-gradient-hero overflow-hidden relative rounded-xl 3xl:h-[77vh] md:h-[67vh] sm:h-[73vh] xs:h-[74vh]">
           <Shapes />
         </div>
