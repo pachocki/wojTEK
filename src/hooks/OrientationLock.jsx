ff53589f75d0962c7602d38dda49318145f28952
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-06-08 13:02:23 +0200

    Update

--- src/hooks/OrientationLock.jsx
+++ src/hooks/OrientationLock.jsx
@@ -4,11 +4,24 @@ const OrientationLock = () => {
   const [isPortrait, setIsPortrait] = useState(true);
 
   useEffect(() => {
+    const isMobileDevice =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    if (!isMobileDevice) {
+      return; // Exit early if not a mobile device
+    }
+
     const lockOrientation = () => {
       const isCurrentlyPortrait = window.innerWidth < window.innerHeight;
 
       if (!isCurrentlyPortrait) {
-        if (window.screen && window.screen.orientation && window.screen.orientation.lock) {
+        if (
+          window.screen &&
+          window.screen.orientation &&
+          window.screen.orientation.lock
+        ) {
           window.screen.orientation
             .lock("portrait")
             .then(() => setIsPortrait(true))
@@ -33,7 +46,11 @@ const OrientationLock = () => {
     };
   }, []);
 
-  return !isPortrait ? <div className="w-full h-screen overflow-hidden bg-black z-[9999] fixed flex justify-center items-center text-5xl"><h1>This site works only in portrait</h1></div> : null;
+  return !isPortrait ? (
+    <div className="w-full h-screen overflow-hidden bg-black z-[9999] fixed flex justify-center items-center text-5xl">
+      <h1>This site works only in portrait</h1>
+    </div>
+  ) : null;
 };
 
 export default OrientationLock;
